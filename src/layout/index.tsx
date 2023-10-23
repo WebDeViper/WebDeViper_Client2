527a1dd7e16366dba1924760d42a0da62ac3bcbe
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2023-10-23 18:16:52 +0900

    feat: 폴더 구조 설정

---
+++ src/layout/index.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from 'react-router-dom';
+import Header from './Header';
+import Main from './Main';
+
+export default function Layout() {
+  return (
+    <div>
+      <Header />
+      <Main>
+        <Outlet />
+      </Main>
+    </div>
+  );
+}
