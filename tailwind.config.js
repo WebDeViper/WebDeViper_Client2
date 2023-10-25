a5337587d12c4f682b0f388849ddca6b90b5dac0
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2023-10-25 14:56:45 +0900

    style: tailwindcss 기본 색상 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      primary: '#22a3cc',
+      semi_primary: '#bce3ef',
+      secondary: '#bfd9ff',
+    },
   },
   plugins: [],
 };
