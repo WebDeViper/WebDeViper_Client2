bad0b284a66ee5901e39174c65f55619f017f290
seokmin <msm0748@gmail.com>
2023-10-26 16:57:53 +0900

    feat: 페이지 경로 설정 및 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/layout';
 import MainPage from './pages/MainPage';
+import RankingPage from './pages/RankingPage';
+import SettingPage from './pages/SettingPage';
+import NoticePage from './pages/NoticePage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<MainPage />} />
+
+        <Route path="/notice" element={<NoticePage />} />
+        <Route path="/ranking" element={<RankingPage />} />
+        <Route path="/setting" element={<SettingPage />} />
       </Route>
     </Routes>
   );
