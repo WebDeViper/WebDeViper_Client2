bfb279a907cc61ec39fb2e9219c878254bf61967
Taehoon <mysoul98@gmail.com>
2023-11-03 17:41:29 +0900

    fix: 내 스터디 그룹 소개글 줄바꿈 안되는 현상 수정

--- src/pages/MainPage/MyGroup/GroupItem.jsx
+++ src/pages/MainPage/MyGroup/GroupItem.jsx
@@ -4,6 +4,11 @@ export default function GroupItem({ subject, imagePath, category, description })
   console.log(description);
   // const editedDescription = description && description?.length > 15 ? description?.slice(0, 14).join('') : description;
   // console.log(editedDescription, '뭔데이거');
+  const maxLength = 20; // 원하는 최대 길이
+  const truncatedDescription =
+    description && description.length > maxLength
+      ? description.slice(0, maxLength) + '...' // 긴 경우 잘라내고 '...'을 추가
+      : description; // 길이가 작은 경우 그대로 둡니다
   return (
     <Link to="/">
       <div className="rounded-[4px] shadow-xl px-16 pb-5 pt-[25px]">
@@ -13,7 +18,7 @@ export default function GroupItem({ subject, imagePath, category, description })
           </div>
           <div className="mt-4 text-center">
             <h3 className="font-bold">{subject}</h3>
-            <p>{description}</p>
+            <p style={{ overflowWrap: 'anywhere' }}>{truncatedDescription}</p>
           </div>
           <div className="mt-2.5">
             <span className="block font-bold">{category}</span>
