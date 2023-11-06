2c6af6f0597c43abaac3da65dfd66cd68ed9ea06
HongMinYeong <minyung1240@daum.net>
2023-11-06 22:36:58 +0900

    feat: 그룹 신청 기능

--- src/pages/MainPage/MyGroup/index.jsx
+++ src/pages/MainPage/MyGroup/index.jsx
@@ -6,92 +6,6 @@ import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
 import { API } from '../../../utils/axios';
 import { useEffect, useState } from 'react';
 
-const data = [
-  {
-    group_id: 1,
-    name: '열공파이아 내 머릿속을 boom boom boom',
-    password: '1234',
-    group_category_name: '대학생',
-    group_image_path:
-      'https://i.namu.wiki/i/tJ3mYQ4YfWJeo5E8ffXEMsN07BuOcBHW04v0gxBlyU1vTQKao0Ag4eKJPQOn3FBeOIWOjNsvTJ6rN-kYk3Mhv2Ee5erFNkwCRLGpwodnH3B4g10aq82OVF39iwLrnqCEVGwWmyR5bag_b3RQPiRWLA.webp',
-    description: '1아니면 0으로 공부합니다.',
-    daily_goal_time: 3,
-    maximum_number_member: 2,
-    is_camera_on: true,
-    createdAt: '2023-10-26T20:20:23.000Z',
-    updatedAt: '2023-10-26T20:20:23.000Z',
-  },
-  {
-    group_id: 2,
-    name: '요리고수가 되고싶은자 나에게로',
-    password: 'alsdud1106',
-    group_category_name: '초등학생',
-    group_image_path:
-      'https://i.namu.wiki/i/tJ3mYQ4YfWJeo5E8ffXEMsN07BuOcBHW04v0gxBlyU1vTQKao0Ag4eKJPQOn3FBeOIWOjNsvTJ6rN-kYk3Mhv2Ee5erFNkwCRLGpwodnH3B4g10aq82OVF39iwLrnqCEVGwWmyR5bag_b3RQPiRWLA.webp',
-    description: '남자가 내릴것은, 달콤한 커피뿐입니다.,,,',
-    daily_goal_time: 7,
-    maximum_number_member: 5,
-    is_camera_on: true,
-    createdAt: '2023-10-26T11:23:29.000Z',
-    updatedAt: '2023-10-26T11:23:29.000Z',
-  },
-  {
-    group_id: 2,
-    name: '요리고수가 되고싶은자 나에게로',
-    password: 'alsdud1106',
-    group_category_name: '초등학생',
-    group_image_path:
-      'https://i.namu.wiki/i/tJ3mYQ4YfWJeo5E8ffXEMsN07BuOcBHW04v0gxBlyU1vTQKao0Ag4eKJPQOn3FBeOIWOjNsvTJ6rN-kYk3Mhv2Ee5erFNkwCRLGpwodnH3B4g10aq82OVF39iwLrnqCEVGwWmyR5bag_b3RQPiRWLA.webp',
-    description: '남자가 내릴것은, 달콤한 커피뿐입니다.,,,',
-    daily_goal_time: 7,
-    maximum_number_member: 5,
-    is_camera_on: true,
-    createdAt: '2023-10-26T11:23:29.000Z',
-    updatedAt: '2023-10-26T11:23:29.000Z',
-  },
-  {
-    group_id: 2,
-    name: '요리고수가 되고싶은자 나에게로',
-    password: 'alsdud1106',
-    group_category_name: '초등학생',
-    group_image_path:
-      'https://i.namu.wiki/i/tJ3mYQ4YfWJeo5E8ffXEMsN07BuOcBHW04v0gxBlyU1vTQKao0Ag4eKJPQOn3FBeOIWOjNsvTJ6rN-kYk3Mhv2Ee5erFNkwCRLGpwodnH3B4g10aq82OVF39iwLrnqCEVGwWmyR5bag_b3RQPiRWLA.webp',
-    description: '남자가 내릴것은, 달콤한 커피뿐입니다.,,,',
-    daily_goal_time: 7,
-    maximum_number_member: 5,
-    is_camera_on: true,
-    createdAt: '2023-10-26T11:23:29.000Z',
-    updatedAt: '2023-10-26T11:23:29.000Z',
-  },
-  {
-    group_id: 2,
-    name: '요리고수가 되고싶은자 나에게로',
-    password: 'alsdud1106',
-    group_category_name: '초등학생',
-    group_image_path:
-      'https://i.namu.wiki/i/tJ3mYQ4YfWJeo5E8ffXEMsN07BuOcBHW04v0gxBlyU1vTQKao0Ag4eKJPQOn3FBeOIWOjNsvTJ6rN-kYk3Mhv2Ee5erFNkwCRLGpwodnH3B4g10aq82OVF39iwLrnqCEVGwWmyR5bag_b3RQPiRWLA.webp',
-    description: '남자가 내릴것은, 달콤한 커피뿐입니다.,,,',
-    daily_goal_time: 7,
-    maximum_number_member: 5,
-    is_camera_on: true,
-    createdAt: '2023-10-26T11:23:29.000Z',
-    updatedAt: '2023-10-26T11:23:29.000Z',
-  },
-  {
-    group_id: 2,
-    name: '요리고수가 되고싶은자 나에게로',
-    password: 'alsdud1106',
-    group_category_name: '초등학생',
-    group_image_path:
-      'https://i.namu.wiki/i/tJ3mYQ4YfWJeo5E8ffXEMsN07BuOcBHW04v0gxBlyU1vTQKao0Ag4eKJPQOn3FBeOIWOjNsvTJ6rN-kYk3Mhv2Ee5erFNkwCRLGpwodnH3B4g10aq82OVF39iwLrnqCEVGwWmyR5bag_b3RQPiRWLA.webp',
-    description: '남자가 내릴것은, 달콤한 커피뿐입니다.,,,',
-    daily_goal_time: 7,
-    maximum_number_member: 5,
-    is_camera_on: true,
-    createdAt: '2023-10-26T11:23:29.000Z',
-    updatedAt: '2023-10-26T11:23:29.000Z',
-  },
-];
 export default function MyGroup() {
   const [myGroups, setMyGroups] = useState([]);
   useEffect(() => {
@@ -127,16 +41,18 @@ export default function MyGroup() {
             const { _id, group_name, group_category, group_image_path, group_description } = item;
             // const roomId = rooms.find(room => room.group === _id)._id;
             return (
-              <SwiperSlide key={index} className="">
-                <GroupItem
-                  // roomId={roomId}
-                  group_id={_id}
-                  imagePath={group_image_path}
-                  subject={group_name}
-                  category={group_category}
-                  description={group_description}
-                />
-              </SwiperSlide>
+              <>
+                <SwiperSlide key={index} className="">
+                  <GroupItem
+                    // roomId={roomId}
+                    group_id={_id}
+                    imagePath={group_image_path}
+                    subject={group_name}
+                    category={group_category}
+                    description={group_description}
+                  />
+                </SwiperSlide>
+              </>
             );
           })}
         </Swiper>
