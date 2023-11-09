58110947643f13928ee4b2ed668eaa2ab0b30324
taehoon <mysoul98@gmail.com>
2023-11-10 02:16:46 +0900

    feat & styles: 랭킹 페이지 시간 형식 00:00:00 수정 및 스타일링

--- src/pages/RankingPage/GroupRank/index.jsx
+++ src/pages/RankingPage/GroupRank/index.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 
-export default function GroupRank({ groupRanking }) {
+export default function GroupRank({ groupRanking, calculateTime }) {
   return (
     <div className="groupRankingWrap">
       <div className="groupRanking">
-        <div className="top3 flex md:flex-row flex-col md:justify-evenly md:mb-5">
+        <div className="top3 flex gap-3 md:flex-row flex-col md:justify-evenly md:mb-5">
           {groupRanking?.slice(0, 3)?.map((group, index) => (
             <div
               key={index}
-              className="flex justify-center md:items-center sm:w-1/4 mb-3 border-2 rounded-lg py-2 px-10"
+              className={`rank${
+                index + 1
+              } flex justify-between md:items-center sm:w-1/3 mb-3 shadow-xl rounded-lg py-2`}
             >
-              <div className="groupInfoWrap flex flex-col items-center">
-                <span>{index + 1}등</span>
+              <div className="w-1/2 flex justify-center">
+                <img
+                  className="w-3/4 h-full rounded-lg"
+                  src={import.meta.env.VITE_APP_BACK_URL + group.group_image_path}
+                  alt="그룹 이미지"
+                />
+              </div>
+              <div className="userInfoWrap flex md:flex-col w-1/2 gap-5 lg:gap-0 items-center">
+                <span className="font-extrabold text-lg">{index + 1}등</span>
                 <span className="font-bold">{group.group_name}</span>
-                <span>{group.groupTotalTime ? group.groupTotalTime : '00:00:00'}</span>
+                <span>{group.groupTotalTime ? calculateTime(group.groupTotalTime) : '00:00:00'}</span>
               </div>
             </div>
           ))}
@@ -22,15 +31,17 @@ export default function GroupRank({ groupRanking }) {
           {groupRanking?.slice(3)?.map(group => (
             <div
               key={Math.random() * 1000000000000}
-              className="h-20 md:max-w-full flex border-2 items-center rounded-lg gap-2 p-4"
+              className="h-20 md:max-w-full flex shadow-lg items-center rounded-lg gap-2 p-4"
             >
               <img
-                // src={import.meta.env.VITE_APP_BACK_URL + '그룹 이미지 경로'}
+                src={import.meta.env.VITE_APP_BACK_URL + group.group_image_path}
                 alt="그룹 이미지"
                 className="h-full rounded-lg"
               />
-              <span className="font-bold">{group.group_name}</span>
-              <span>{group.averageTime || '00:00:00'}</span>
+              <div className="flex flex-col">
+                <span className="font-bold">{group.group_name}</span>
+                <span>{group.groupTotalTime ? calculateTime(group.groupTotalTime) : '00:00:00'}</span>
+              </div>
             </div>
           ))}
         </div>
--- src/pages/RankingPage/UserRank/index.jsx
+++ src/pages/RankingPage/UserRank/index.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
 
-export default function UserRank({ userRanking }) {
+export default function UserRank({ userRanking, calculateTime }) {
   return (
     <div className="userRankingWrap mb-5">
       <div className="userRanking">
-        <div className="top3 flex md:flex-row flex-col md:justify-evenly md:mb-5">
+        <div className="top3 flex gap-3 md:flex-row flex-col md:justify-evenly md:mb-5">
           {userRanking?.slice(0, 3)?.map((user, index) => (
             <div
               key={index}
-              className="flex justify-between md:items-center xs:w-1/4 mb-3 border-2 rounded-lg py-2 px-10"
+              className={`rank${
+                index + 1
+              } flex justify-between md:items-center sm:w-1/3 mb-3 shadow-xl rounded-lg py-2`}
             >
-              <div className="userImg">
+              <div className="w-1/2 flex justify-center">
                 <img
-                  className="w-20 h-20"
+                  className="w-3/4 h-full rounded-lg"
                   src={import.meta.env.VITE_APP_BACK_URL + user.user_profile_image_path}
-                  alt=""
+                  alt="유저 이미지"
                 />
               </div>
-              <div className="userInfoWrap flex flex-col items-center">
-                <span>{index + 1}등</span>
+              <div className="userInfoWrap flex md:flex-col w-1/2 gap-5 lg:gap-0 items-center">
+                <span className="font-extrabold text-lg">{index + 1}등</span>
                 <span className="font-bold">{user.user_nickname}</span>
-                <span>{user.user_total_time ? user.user_total_time : '00:00:00'}</span>
+                <span>{user.user_total_time ? calculateTime(user.user_total_time) : '00:00:00'}</span>
               </div>
             </div>
           ))}
@@ -29,15 +31,17 @@ export default function UserRank({ userRanking }) {
           {userRanking?.slice(3)?.map(user => (
             <div
               key={user.user_nickname}
-              className="h-20 md:max-w-full flex border-2 items-center rounded-lg gap-2 p-4"
+              className="h-20 md:max-w-full flex shadow-lg items-center rounded-lg gap-2 p-4"
             >
               <img
                 src={import.meta.env.VITE_APP_BACK_URL + user.user_profile_image_path}
                 alt="유저 이미지"
                 className="h-full rounded-lg"
               />
-              <span className="font-bold">{user.user_nickname}</span>
-              <span>{user.user_total_time || '00:00:00'}</span>
+              <div className="flex flex-col">
+                <span className="font-bold">{user.user_nickname}</span>
+                <span>{user.user_total_time ? calculateTime(user.user_total_time) : '00:00:00'}</span>
+              </div>
             </div>
           ))}
         </div>
